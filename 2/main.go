45ddf85875249a2b887079ba4d7575f2fb07adb8
VFSVINI <vfs2@cin.ufpe.br>
2025-08-27 22:20:45 -0300

    Declarando Variáveis

---
+++ 2/main.go
@@ -0,0 +1,23 @@
+package main
+
+const a = "Hello, World"
+
+var (
+	b bool    = true
+	c int     = 23
+	d string  = "PG"
+	e float64 = 2025.2
+)
+
+func main() {
+
+	a := "X" //STRING
+	a = "Y"
+
+	println(a)
+
+}
+
+func x() {
+
+}
