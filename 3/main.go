46497b1a1ceb5554e1787912c1469490ac2dc8f8
VFSVINI <vfs2@cin.ufpe.br>
2025-08-27 22:36:20 -0300

    Criação de Tipos

---
+++ 3/main.go
@@ -0,0 +1,19 @@
+package main
+
+const a = "Hello, World"
+
+type ID int
+
+var (
+	b bool    = true
+	c int     = 23
+	d string  = "PG"
+	e float64 = 2025.2
+	f ID      = 1
+)
+
+func main() {
+
+	println(f)
+
+}
